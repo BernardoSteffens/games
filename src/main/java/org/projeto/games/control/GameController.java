a1fb2b8663a10730d8c82313ed8946d11bb4abf6
bernardo <85709693+BernardoSteffens@users.noreply.github.com>
2025-11-17 10:55:55 -0300

    alterado permissão de acesso

--- src/main/java/org/projeto/games/control/GameController.java
+++ src/main/java/org/projeto/games/control/GameController.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 @RequestMapping("/games")
 @RestController
 public class GameController {
